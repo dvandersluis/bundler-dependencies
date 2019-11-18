55eaeda469b6b595d8c3ed69cbe02edd3f6f159d
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2019-11-18 10:17:33 -0500

    Clean up repeated options

--- lib/bundler/dependencies/cli.rb
+++ lib/bundler/dependencies/cli.rb
@@ -7,23 +7,25 @@ require 'bundler/dependencies'
 module Bundler
   module Dependencies
     class CLI < ::Thor
+      def self.shared_options
+        method_option :path, type: :string, desc: 'Path to Gemfile.lock to scan'
+        method_option :without, type: :array, desc: 'Gems to ignore', aliases: ['-W']
+        method_option :without_rails, type: :boolean, default: false, desc: 'Ignore all Rails gems', aliases: ['-R']
+      end
+
       default_task :count
       map '--version' => :version
 
       desc 'count', 'Checks for gems that install too many dependencies'
-      method_option :path, type: :string, desc: 'Path to Gemfile.lock to scan', default: Bundler.default_lockfile
-      method_option :minimum, type: :numeric, desc: 'Report only gems with a minimum N dependencies', aliases: ['-m', '--min'], default: 0
-      method_option :without, type: :array, desc: 'Gems to ignore', aliases: ['-W']
-      method_option :without_rails, type: :boolean, default: false, desc: 'Ignore all Rails gems', aliases: ['-R']
+      shared_options
+      method_option :minimum, type: :numeric, desc: 'Report only gems with a minimum N dependencies', aliases: ['-m'], default: 0
 
       def count
         Count.new(options).output
       end
 
       desc 'graph [GEM]', 'Outputs a dependency graph'
-      method_option :path, type: :string, desc: 'Path to Gemfile.lock to scan', default: Bundler.default_lockfile
-      method_option :without, type: :array, desc: 'Gems to ignore', aliases: ['-W']
-      method_option :without_rails, type: :boolean, default: false, desc: 'Ignore all Rails gems', aliases: ['-R']
+      shared_options
 
       def graph(gem = nil)
         Graph.new(gem, options).output
